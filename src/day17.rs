cd4caa468ebc440fd3d49b7c9aca5b1e8e1c919d
matze <>
2024-12-17 11:14:30 +0100

    day 17 slow-ish

--- src/day17.rs
+++ src/day17.rs
@@ -24,7 +24,7 @@ fn run(program: &Vec<i128>, reg_a: i128, reg_b: i128, reg_c: i128, inst: usize, 
             0 => {
                 reg_a /= (1 << co);
                 assert_eq!(co, 3);
-                max_bit_read = max_bit_read.max(bits_discarded + (co as usize));
+                //max_bit_read = max_bit_read.max(bits_discarded + (co as usize));
                 bits_discarded += 3;
                 //println!("discarded3 {}", reg_a);
                 //println!("AA{:?} {}", reg_a, (1 << co));
@@ -53,18 +53,14 @@ fn run(program: &Vec<i128>, reg_a: i128, reg_b: i128, reg_c: i128, inst: usize, 
                 if output.len() == n_outputs {
                     return (output, bits_discarded, max_bit_read);
                 }
-                // println!("{:?}", output);
                 inst += 2;
             }
             6 => {
                 reg_b = reg_a / (1 << co);
-                max_bit_read = max_bit_read.max(bits_discarded + 3 + (co as usize));
                 inst += 2;
             }
             7 => {
                 reg_c = reg_a / (1 << co);
-                max_bit_read = max_bit_read.max(bits_discarded + 3 + (co as usize));
-                //println!("read {}", co);
                 inst += 2;
             }
             _ => unreachable!(),
@@ -75,41 +71,24 @@ fn run(program: &Vec<i128>, reg_a: i128, reg_b: i128, reg_c: i128, inst: usize, 
 }
 
 pub(crate) fn solve(data: &str) -> (i64, i64) {
-    let data_ = "Register A: 729
-Register B: 0
-Register C: 0
-
-Program: 0,1,5,4,3,0";
     let reg = Regex::new(r"(-?\d+)").unwrap();
     let nums = reg.find_iter(data).map(|x| x.as_str().parse().unwrap()).collect::<Vec<i128>>();
 
-    let mut reg_a = nums[0];
-    let mut reg_b = nums[1];
-    let mut reg_c = nums[2];
-
-    let mut inst = 0;
 
     let program = nums[3..].to_vec();
-    println!("{:?}", program.len());
+    let (p1, _, _) = run(&program, nums[0], nums[1], nums[2], 0, program.len());
+    println!("{:?}", p1);
 
-    // let co = |op: i128| -> i128{
-    //     match op {
-    //         x @ 0..=3 => x,
-    //         4 => reg_a,
-    //         5 => reg_b,
-    //         6 => reg_c,
-    //         _ => unreachable!(),
-    //     }
-    // };
 
     let mut candidates = vec![(0, 0)];
     let mut gotchas = HashSet::new();
     for i in 1..program.len() + 1 {
         let mut nc = HashSet::new();
         for &(low, discarded) in candidates.iter() {
-            for reg_a in 1..(1 << (13)) {
+            // 10 bit seems to be enough
+            for reg_a in 1..(1 << 10) {
                 let reg_a = (reg_a << discarded) + low;
-                let (output, bits_discarded, max_bit_read) = run(&program, reg_a, reg_b, reg_c, inst, i);
+                let (output, bits_discarded, _max_bit_read) = run(&program, reg_a, 0, 0, 0, i);
                 if output == program[..output.len()] {
                     if output == program {
                         gotchas.insert(reg_a);
@@ -121,10 +100,10 @@ Program: 0,1,5,4,3,0";
         candidates = nc.into_iter().collect();
     }
 
-    println!("GOTCHA:{}", gotchas.iter().min().unwrap());
+    // println!("GOTCHA:{}", gotchas.iter().min().unwrap());
 
     // let mut i = 0;
     // println!("{:?}", output);
 
-    (0, 0)
+    (0, *gotchas.iter().min().unwrap() as i64)
 }
