9d8a26c065f4fb4347d2b8fdb7b1146509ffcaee
matze <>
2024-12-17 04:31:10 +0100

    day 16 fixes

--- src/day16.rs
+++ src/day16.rs
@@ -1,5 +1,4 @@
 use crate::str_map::StrMap;
-use hashbrown::{HashMap, HashSet};
 use priority_queue::PriorityQueue;
 use std::cmp::Reverse;
 
@@ -9,10 +8,12 @@ type D16State = (i32, i32, usize);
 fn get_best_pred_counts(
     end_state: D16State,
     m: &StrMap,
-    distances: &HashMap<D16State, i32>,
+    distances: &Vec<i32>,
+    w: i32,
+    h: i32,
 ) -> i64 {
     let mut frontier = Vec::new();
-    let mut visited_crossings: HashSet<D16State> = HashSet::new();
+    let mut visited_crossings = vec![0; (h * w) as usize];
     frontier.push(end_state);
     let mut res = 0;
     while !frontier.is_empty() {
@@ -26,21 +27,19 @@ fn get_best_pred_counts(
             // prevent recount if already found
             let mut found = false;
             for rot in [1, 2, 3] {
-                let mut expected_cost = distances[&(x, y, r)] - cost - 1;
+                let mut expected_cost = distances[((x * w + y) * 4) as usize + r] - cost - 1;
                 if rot != 2 {
                     expected_cost -= 1000;
                 }
-                let pred = (cx, cy, (cr + rot) % 4);
-                if distances.contains_key(&pred) {
-                    if distances[&pred] == expected_cost {
-                        if !visited_crossings.contains(&pred) {
-                            visited_crossings.insert(pred);
-                            frontier.push(pred);
-                        }
-                        if !found {
-                            res += counts;
-                            found = true;
-                        }
+                let pred_rot = (cr + rot) % 4;
+                if distances[((cx * w + cy) * 4) as usize + pred_rot] == expected_cost {
+                    if visited_crossings[(cx * w + cy) as usize] & (1 << pred_rot) == 0 {
+                        visited_crossings[(cx * w + cy) as usize] |= (1 << pred_rot);
+                        frontier.push((cx, cy, pred_rot));
+                    }
+                    if !found {
+                        res += counts;
+                        found = true;
                     }
                 }
             }
@@ -48,10 +47,8 @@ fn get_best_pred_counts(
     }
     let rs = visited_crossings
         .iter()
-        .map(|y| (y.0, y.1))
-        .collect::<HashSet<_>>()
-        .len() as i64;
-    res as i64 + rs + 1
+        .filter(|&&s| s != 0).count();
+    res as i64 + (rs as i64) + 1
 }
 
 fn step_to_next_crossing(m: &StrMap, x: i32, y: i32, i: usize) -> Option<(D16State, i32, i32)> {
@@ -59,7 +56,7 @@ fn step_to_next_crossing(m: &StrMap, x: i32, y: i32, i: usize) -> Option<(D16Sta
     let mut cost = 0;
     let mut dist = 0;
     loop {
-        let mut nb: Option<(i32, i32, usize)> = None;
+        let mut nb: Option<D16State> = None;
         let curr_sym = m.get(curr.0, curr.1);
         if curr_sym == b'E' || curr_sym == b'S' {
             return Some((curr, cost, dist));
@@ -73,6 +70,7 @@ fn step_to_next_crossing(m: &StrMap, x: i32, y: i32, i: usize) -> Option<(D16Sta
             let (nnx, nny) = (curr.0 + ddx, curr.1 + ddy);
             if m.get(nnx, nny) != b'#' {
                 if nb.is_some() {
+                    // second neighbor detected => return crossing coords
                     return Some((curr, cost, dist));
                 } else {
                     nb = Some((nnx, nny, j))
@@ -109,16 +107,17 @@ pub(crate) fn solve(data: &str) -> (i64, i64) {
         h,
     };
 
-    let mut distances: HashMap<D16State, i32> = HashMap::new();
+    let mut distances = vec![-1; (h * w * 4) as usize];
     let mut frontier: PriorityQueue<D16State, _> = PriorityQueue::new();
-    frontier.push((start_pos.0, start_pos.1, 0usize), Reverse(0));
+    frontier.push((start_pos.0, start_pos.1, 0), Reverse(0));
     loop {
         let ((x, y, r), dist) = frontier.pop().unwrap();
-        distances.insert((x, y, r), dist.0);
+        distances[((x * w + y) * 4) as usize + r] = dist.0;
         if (x, y) == end_pos {
             return (
                 dist.0 as i64,
-                get_best_pred_counts((x, y, r), &m, &distances),
+                // 0
+                get_best_pred_counts((x, y, r), &m, &distances, w, h),
             );
         }
         for rot in [0, 1, 3] {
@@ -128,7 +127,7 @@ pub(crate) fn solve(data: &str) -> (i64, i64) {
                 continue;
             }
             step_to_next_crossing(&m, nx, ny, new_rot).map(|(s, cost, counts)| {
-                if !distances.contains_key(&s) {
+                if distances[((s.0 * w + s.1) * 4) as usize + s.2] == -1 {
                     let new_dist = 1 + dist.0 + cost + (rot % 2) as i32 * 1000;
                     frontier.push_increase(s, Reverse(new_dist));
                 }
